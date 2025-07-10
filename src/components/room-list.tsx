86187ad1405302bfe989eff4218e44e2cbd8f833
Holices Junior <dev.holices@gmail.com>
2025-07-10 11:06:08 +0100

    feat: move room-list to components file

---
+++ src/components/room-list.tsx
@@ -0,0 +1,70 @@
+import { useQuery } from '@tanstack/react-query'
+import { ArrowRight } from 'lucide-react'
+import { Link } from 'react-router-dom'
+import type { GetRoomsResponse } from '@/http/types/get-rooms-response'
+import { dayjs } from '@/lib/dayjs'
+import { Badge } from './ui/badge'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+
+export function RoomList() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+      const result: GetRoomsResponse = await response.json()
+
+      return result
+    },
+  })
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sala recentes</CardTitle>
+        <CardDescription>
+          Acesso rápido para as salas criadas recentemente
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-3">
+        {isLoading && (
+          <p className="text-muted-foreground text-sm">Carregando salas...</p>
+        )}
+
+        {data?.map((room) => {
+          return (
+            <Link
+              className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent/50"
+              key={room.id}
+              to={`/rooms/${room.id}`}
+            >
+              <div className="flex flex-1 flex-col gap-1">
+                <h3 className="font-medium">{room.name}</h3>
+
+                <div className="flex items-center gap-2">
+                  <Badge className="text-xs" variant="secondary">
+                    {dayjs(room.createdAt).toNow()}
+                  </Badge>
+
+                  <Badge className="text-xs" variant="secondary">
+                    {room.questionsCount} pergunta(s)
+                  </Badge>
+                </div>
+              </div>
+
+              <span className="flex items-center gap-1 text-sm">
+                Entrar
+                <ArrowRight className="size-3" />
+              </span>
+            </Link>
+          )
+        })}
+      </CardContent>
+    </Card>
+  )
+}
