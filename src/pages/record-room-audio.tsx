c43a913c18411fec5b474e68600b919bfadaa643
Holices Junior <dev.holices@gmail.com>
2025-07-21 22:33:59 +0100

    feat: add intervalRef

--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -6,7 +6,6 @@ const isRecordingSupported =
   !!navigator.mediaDevices &&
   typeof navigator.mediaDevices.getUserMedia === 'function' &&
   typeof window.MediaRecorder === 'function'
-
 type RoomParams = {
   roomId: string
 }
@@ -15,12 +14,17 @@ export function RecordRoomAudio() {
   const params = useParams<RoomParams>()
   const [isRecording, setIsRecording] = useState(false)
   const recorder = useRef<MediaRecorder | null>(null)
+  const intervalRef = useRef<NodeJS.Timeout>(null)
 
   function stopRecording() {
     setIsRecording(false)
 
     if (recorder.current && recorder.current.state !== 'inactive') {
       recorder.current.stop()
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current)
     }
   }
 
@@ -39,7 +43,33 @@ export function RecordRoomAudio() {
 
     const result = await response.json()
 
+    // biome-ignore lint/suspicious/noConsole: stop biome
     console.log(result)
+  }
+
+  function createRecorder(audio: MediaStream) {
+    recorder.current = new MediaRecorder(audio, {
+      mimeType: 'audio/webm',
+      audioBitsPerSecond: 64_000,
+    })
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data)
+      }
+    }
+
+    recorder.current.onstart = () => {
+      // biome-ignore lint/suspicious/noConsole: stop biome
+      console.log('Gravação iniciada')
+    }
+
+    recorder.current.onstop = () => {
+      // biome-ignore lint/suspicious/noConsole: stop biome
+      console.log('Gravação finalizada.')
+    }
+
+    recorder.current.start()
   }
 
   async function startRecording() {
@@ -58,26 +88,13 @@ export function RecordRoomAudio() {
       },
     })
 
-    recorder.current = new MediaRecorder(audio, {
-      mimeType: 'audio/webm',
-      audioBitsPerSecond: 64_000,
-    })
+    createRecorder(audio)
 
-    recorder.current.ondataavailable = (event) => {
-      if (event.data.size > 0) {
-        uploadAudio(event.data)
-      }
-    }
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop()
 
-    recorder.current.onstart = () => {
-      console.log('Gravação iniciada')
-    }
-
-    recorder.current.onstop = () => {
-      console.log('Gravação finalizada.')
-    }
-
-    recorder.current.start()
+      createRecorder(audio)
+    }, 5000)
   }
 
   if (!params.roomId) {
